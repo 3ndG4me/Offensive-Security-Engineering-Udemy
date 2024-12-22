25ae6285ffe8a87d506ba35498ddd80c685a8673
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2024-12-22 15:54:10 -0500

    refractor

--- serverless-redirector/flask-redirector/app.py
+++ serverless-redirector/flask-redirector/app.py
@@ -5,60 +5,41 @@ import urllib3
 http = urllib3.PoolManager()
 app = Flask(__name__)
 
-host = "https://injection.sh"
+def redirect(path):
+    query = request.query_string.decode()
+    
+    host = "https://injection.sh"
+
+    if path != '':
+        host += "/" + path
+
+    if query != '':
+        host += "?" + query # Replace with your API URL
+
+    try:
+        # Make the GET request
+        response = http.request('GET', host)
+
+        return {
+            'statusCode': 200,
+            'body':  response.data.decode('utf-8') # Decode the response from bytes to string
+        }
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
 
 @app.route("/")
 def index_route():
-    query = request.query_string.decode()
-
-    url = host 
-
-    if query != '':
-        url += "?" + query # Replace with your API URL
-
-    try:
-        # Make the GET request
-        response = http.request('GET', url)
-
-        return {
-            'statusCode': 200,
-            'body':  response.data.decode('utf-8') + "url was:" + url # Decode the response from bytes to string
-        }
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                'error': str(e)
-            })
-        }
+    return redirect('')
 
 @app.route('/<path:path>')
 def catch_all(path):
-
-    query = request.query_string.decode()
-
-    url = host + "/" + path
-
-    if query != '':
-        url += "?" + query # Replace with your API URL
-
-    try:
-        # Make the GET request
-        response = http.request('GET', url)
-
-        return {
-            'statusCode': 200,
-            'body':  response.data.decode('utf-8') + "url was:" + url # Decode the response from bytes to string
-        }
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                'error': str(e)
-            })
-        }
+    return redirect(path)
 
 @app.errorhandler(404)
 def resource_not_found(e):
